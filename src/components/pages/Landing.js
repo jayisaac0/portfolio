443ac992caa0158a2ff1935be3b421df1b3ed026
jayisaac0 <jayisaac0@gmail.com>
2020-02-10 01:08:52 +0300

    app

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import Banner from '../info/Banner';
 import SectionOne from '../body/SectionOne';
-// import SectionTwo from './body/SectionTwo';
-// import SectionThree from '../body/SectionThree';
-// import SectionFour from '../body/SectionFour';
-// import SectionFive from '../body/SectionFive';
 
 class Landing extends Component {
     state = {  }
